350ed1051f7654043bc62eef581140df5777b6f6
shivani udgire <shivani@sujainfo.net>
2025-12-15 00:21:08 +0530

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,7 +20,8 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().
+WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
